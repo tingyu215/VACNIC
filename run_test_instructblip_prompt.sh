24b71301d0008df801625e8b8b04ea422662510d
tingyu215 <tingyuqu95@outlook.com>
2024-04-01 21:09:23 +0200

    code update

--- run_test_instructblip_prompt.sh
+++ run_test_instructblip_prompt.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-CUDA_VISIBLE_DEVICES=0 python /CODEDIR/test_instructblip_prompt.py --num_workers 8 --batch_size 1 --use_opt False --use_retrieval True --data_type nytimes
-
 CUDA_VISIBLE_DEVICES=0 python /CODEDIR/test_instructblip_prompt.py --num_workers 8 --batch_size 1 --use_opt False --use_retrieval True --data_type goodnews
+
+
+CUDA_VISIBLE_DEVICES=0 python /CODEDIR/test_instructblip_prompt.py --num_workers 8 --batch_size 1 --use_opt False --use_retrieval True --data_type nytimes
--- run_test_llava_prompt.sh
+++ run_test_llava_prompt.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-CUDA_VISIBLE_DEVICES=2 python /CODEDIR/test_llava_prompt.py --num_workers 8 --batch_size 16 --use_opt False --use_retrieval True --data_type goodnews
+CUDA_VISIBLE_DEVICES=0 python /CODEDIR/test_llava_prompt.py --num_workers 8 --batch_size 16 --use_opt False --use_retrieval True --data_type goodnews
 
-CUDA_VISIBLE_DEVICES=2 python /CODEDIR/test_llava_prompt.py --num_workers 8 --batch_size 16 --use_opt False --use_retrieval True --data_type nytimes
+CUDA_VISIBLE_DEVICES=0 python /CODEDIR/test_llava_prompt.py --num_workers 8 --batch_size 16 --use_opt False --use_retrieval True --data_type nytimes
